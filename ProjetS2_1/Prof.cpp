5d8d985a3c337bbadf7ea50cb2c0de7d27ae6d9f
Caroleannv <carole-ann.villeneuve@usherbrooke.ca>
2019-02-20 13:00:00 -0500

    Push1
    Push1

--- ProjetS2_1/Prof.cpp
+++ ProjetS2_1/Prof.cpp
@@ -1,6 +1,8 @@
 #include "Prof.h"
+#include <iostream>
+#include <string>
 
-
+using namespace std; 
 
 Prof::Prof()
 {
@@ -11,6 +13,96 @@ Prof::~Prof()
 {
 }
 
+void Prof::changerEtat(int etat)
+{
+}
+
+void Prof::attaquer(int sort, Prof ennemi)
+{
+
+}
+
+int Prof::setVie(int niveauVie)
+{
+	vie = niveauVie;
+	return vie;
+}
+
+int Prof::getVie()
+{
+	return vie;
+}
+
+string Prof::setNom(string nomProf)
+{
+	nom = nomProf;
+	return nom;
+}
+
+string Prof::getNom()
+{
+	return nom;
+}
+
+int Prof::getAttaquePuissance()
+{
+	return attaquePuissance;
+}
+
+int Prof::setAttaquePuissance(int attaque)
+{
+	attaquePuissance = attaque;
+	return attaquePuissance;
+}
+
+int Prof::getDeense()
+{
+	return defense;
+}
+
+int Prof::setDefense(int def)
+{
+	defense = def;
+	return defense;
+}
+
+int Prof::getEtatEffet()
+{
+	return etatEffet;
+}
+
+int Prof::setEtatEffet(int etat)
+{
+	etatEffet = etat;
+	if (etat == 1)return etat;
+	if (etat == 2)return etat;
+	if (etat == 3)return etat;
+	if (etat == 4)return etat;
+	return etatEffet;
+}
+
+string Prof::getSprite()
+{
+	return sprite;
+}
+
+Vecteur<Sort> Prof::getSort()
+{
+	return Vecteur<Sort>();
+}
+
 void Prof::afficher() {
-	cout << "test" << endl;
+	cout << "----Test classe Prof----" << endl;
+	
+	cout << "Votre Poke-Prof est : " <<  endl;
+	cout << nom << endl;
+	cout << "Niveau de vie : " << vie << endl;
+	cout << "Defense : " << defense << endl;
+	cout << "Etat : " << endl;
+	if (etatEffet == 1)cout << "Neutre" << endl;
+	if (etatEffet == 2)cout << "Saoul" << endl;
+	if (etatEffet == 3)cout << "Etourdis" << endl;
+	if (etatEffet == 4)cout << "Hyperactif" << endl;
+	
+	
 }
--- ProjetS2_1/Prof.h
+++ ProjetS2_1/Prof.h
@@ -1,12 +1,45 @@
 #include <iostream>
-#pragma once
+#include "vecteur.h"
+#include "sort.h"
+
 
 using namespace std;;
 class Prof
 {
+private:
+	int vie;
+	string nom;
+	int attaquePuissance;
+	int defense;
+	int etatEffet;
+	string sprite;
+	Vecteur<Sort> sort;
+
 public:
 	Prof();
 	virtual ~Prof();
+	void changerEtat(int etat);
+	void attaquer(int sort, Prof ennemi);
+
+	int setVie(int niveauVie);
+	int getVie();
+
+	string setNom(string nomProf);
+	string getNom();
+
+	int getAttaquePuissance();
+	int setAttaquePuissance(int attaque);
+
+	int getDeense();
+	int setDefense(int def);
+
+	int getEtatEffet();
+	int setEtatEffet(int etat);
+
+	string getSprite();
+	
+	Vecteur<Sort> getSort();
+
 	void afficher();
 };
 
